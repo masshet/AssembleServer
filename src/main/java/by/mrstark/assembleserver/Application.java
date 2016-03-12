3b0e9287acbf7481b813544b66e3e32e13928c79
mrstark <lmasliakov@gmail.com>
2016-03-12 04:25:10 +0300

    Add Entity User

---
+++ src/main/java/by/mrstark/assembleserver/Application.java
@@ -0,0 +1,7 @@
+package by.mrstark.assembleserver;
+
+/**
+ * Created by mrstark on 12.3.16.
+ */
+public class Application {
+}
---
+++ src/main/java/by/mrstark/assembleserver/entity/User.java
@@ -0,0 +1,7 @@
+package by.mrstark.assembleserver.entity;
+
+/**
+ * Created by mrstark on 12.3.16.
+ */
+public class User {
+}
